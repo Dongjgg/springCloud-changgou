fcfdbc4f848cca564892f538930d5e9eb63ea04d
Dj <1132580253@qq.com>
2023-08-11 16:29:40 +0800

    规格查询+参数列表查询业务代码开发+测试成功，2023年8月11日

---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/dao/SpecMapper.java
@@ -0,0 +1,11 @@
+package com.changgou.goods.dao;
+import com.changgou.goods.pojo.Spec;
+import tk.mybatis.mapper.common.Mapper;
+
+/****
+ * @Author:Dj
+ * @Description:Spec的Dao
+ * @Date 2023年8月11日
+ *****/
+public interface SpecMapper extends Mapper<Spec> {
+}
