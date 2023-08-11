22e348c70eda4064ccaddece8257ea6a71a46d0c
Dj <1132580253@qq.com>
2023-08-11 16:45:12 +0800

    Sku增删改查业务代码开发+测试成功，2023年8月11日

---
+++ changgou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/dao/SkuMapper.java
@@ -0,0 +1,11 @@
+package com.changgou.goods.dao;
+import com.changgou.goods.pojo.Sku;
+import tk.mybatis.mapper.common.Mapper;
+
+/****
+ * @Author:Dj
+ * @Description:Sku的Dao
+ * @Date 2023年8月11日
+ *****/
+public interface SkuMapper extends Mapper<Sku> {
+}
